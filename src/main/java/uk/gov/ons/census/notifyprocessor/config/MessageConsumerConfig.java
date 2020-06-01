18f3300166fe289720b1004aa8f225684c91cb9a
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2020-06-01 09:56:52 +0100

    Persist quarantined messages (#23)

--- src/main/java/uk/gov/ons/census/notifyprocessor/config/MessageConsumerConfig.java
+++ src/main/java/uk/gov/ons/census/notifyprocessor/config/MessageConsumerConfig.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.AbstractMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -26,7 +25,6 @@ import uk.gov.ons.census.notifyprocessor.model.ResponseManagementEvent;
 @Configuration
 public class MessageConsumerConfig {
   private final ExceptionManagerClient exceptionManagerClient;
-  private final RabbitTemplate rabbitTemplate;
   private final ConnectionFactory connectionFactory;
 
   @Value("${messagelogging.logstacktraces}")
@@ -41,9 +39,6 @@ public class MessageConsumerConfig {
   @Value("${queueconfig.retry-delay}")
   private int retryDelay;
 
-  @Value("${queueconfig.quarantine-exchange}")
-  private String quarantineExchange;
-
   @Value("${queueconfig.fulfilment-request-inbound-queue}")
   private String fulfilmentInboundQueue;
 
@@ -51,11 +46,8 @@ public class MessageConsumerConfig {
   private String enrichedFulfilmentQueue;
 
   public MessageConsumerConfig(
-      ExceptionManagerClient exceptionManagerClient,
-      RabbitTemplate rabbitTemplate,
-      ConnectionFactory connectionFactory) {
+      ExceptionManagerClient exceptionManagerClient, ConnectionFactory connectionFactory) {
     this.exceptionManagerClient = exceptionManagerClient;
-    this.rabbitTemplate = rabbitTemplate;
     this.connectionFactory = connectionFactory;
   }
 
@@ -104,9 +96,7 @@ public class MessageConsumerConfig {
             expectedMessageType,
             logStackTraces,
             "Notify Processor",
-            queueName,
-            quarantineExchange,
-            rabbitTemplate);
+            queueName);
 
     RetryOperationsInterceptor retryOperationsInterceptor =
         RetryInterceptorBuilder.stateless()
