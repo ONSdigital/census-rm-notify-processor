52a7c5ca0ca7ac15d308f74ad2b62e59dffa7292
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-06-15 10:54:34 +0100

    Update QID type names

--- src/main/java/uk/gov/ons/census/notifyprocessor/utilities/TemplateMapper.java
+++ src/main/java/uk/gov/ons/census/notifyprocessor/utilities/TemplateMapper.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class TemplateMapper {
   private static final int HOUSEHOLD_ENGLAND = 1;
-  private static final int HOUSEHOLD_WALES_IN_ENGLISH = 2;
+  private static final int HOUSEHOLD_WALES = 2;
   private static final int HOUSEHOLD_WALES_IN_WELSH = 3;
   private static final int HOUSEHOLD_NI = 4;
   private static final int INDIVIDUAL_RESPONSE_ENGLAND = 21;
-  private static final int INDIVIDUAL_RESPONSE_WALES_IN_ENGLISH = 22;
+  private static final int INDIVIDUAL_RESPONSE_WALES = 22;
   private static final int INDIVIDUAL_RESPONSE_WALES_IN_WELSH = 23;
   private static final int INDIVIDUAL_RESPONSE_NI = 24;
   private final String templateEnglish;
@@ -38,7 +38,7 @@ public class TemplateMapper {
         result = new Tuple(HOUSEHOLD_ENGLAND, templateEnglish);
         break;
       case "UACHHT2":
-        result = new Tuple(HOUSEHOLD_WALES_IN_ENGLISH, templateWelshAndEnglish);
+        result = new Tuple(HOUSEHOLD_WALES, templateWelshAndEnglish);
         break;
       case "UACHHT2W":
         result = new Tuple(HOUSEHOLD_WALES_IN_WELSH, templateWelsh);
@@ -50,7 +50,7 @@ public class TemplateMapper {
         result = new Tuple(INDIVIDUAL_RESPONSE_ENGLAND, templateEnglish);
         break;
       case "UACIT2":
-        result = new Tuple(INDIVIDUAL_RESPONSE_WALES_IN_ENGLISH, templateWelshAndEnglish);
+        result = new Tuple(INDIVIDUAL_RESPONSE_WALES, templateWelshAndEnglish);
         break;
       case "UACIT2W":
         result = new Tuple(INDIVIDUAL_RESPONSE_WALES_IN_WELSH, templateWelsh);
